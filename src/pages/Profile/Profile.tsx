41ca1b1aa06cecbe3dd5608b9c8270a8f422f695
Dairo García Naranjo <50602257+Dairo01001@users.noreply.github.com>
2025-03-05 20:58:36 -0500

    profile-bug

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,13 +1,18 @@
 import { useCookies } from 'react-cookie'
 import { EditPerson, EditProfile } from './components'
 import { useUserMe } from './hooks'
+import { Navigate } from 'react-router-dom'
 
 export const Profile = () => {
   const { me } = useUserMe()
   const [cookie] = useCookies(['user'])
-  const { accessToken } = cookie.user
+  const accessToken = cookie?.user?.accessToken
   if (!me) {
     return <div>Loading...</div>
+  }
+
+  if (!accessToken) {
+    return <Navigate to="/" />
   }
 
   return (
